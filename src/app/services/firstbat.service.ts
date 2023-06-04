8d00ddce9314b62715c1e2f4cd284ecb39bd2505
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-06-04 10:54:39 +0530

    add score card for first bat team

--- src/app/services/firstbat.service.ts
+++ src/app/services/firstbat.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirstbatService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/api/scorecard/first';
+
+  constructor(private http: HttpClient) { }
+
+  getSummary(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 }
