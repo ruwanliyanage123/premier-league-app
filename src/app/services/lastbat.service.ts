97c9f2a134de64ac92878d369c2f966cb3bd4baa
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-06-04 11:18:40 +0530

    add score card for second batting team

--- src/app/services/lastbat.service.ts
+++ src/app/services/lastbat.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LastbatService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/api/scorecard/second'
+
+  constructor(private http: HttpClient) {}
+
+  getLastBattingResult():Observable<any>{
+      return this.http.get<any>(this.url);
+  }
 }
