5cccd7a4d15a8d2ef7c70b741a053da0abc3cd36
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-06-04 15:36:12 +0530

    add score summary

--- src/app/services/match-summary.service.ts
+++ src/app/services/match-summary.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchSummaryService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/api/summary/result';
+
+  constructor(private http: HttpClient) { }
+
+  getMatchSummary() :Observable<any>{
+     return this.http.get<any>(this.url);
+  }
 }
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatchSummaryService } from '../services/match-summary.service';
 
 @Component({
   selector: 'app-summary',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./summary.component.css'],
 })
 export class SummaryComponent {
+   first_batting : any;
+   first_bowling : any;
+   second_batting : any;
+   second_bowling : any;
+   constructor(private service: MatchSummaryService){}
 
+   ngOnInit(){
+    this.getMatchSummary();
+   }
+
+   getMatchSummary(): void{
+     this.service.getMatchSummary()
+     .subscribe((sum)=>{
+        this.first_batting = sum.first_batting;
+        this.first_bowling = sum.first_bowling;
+        this.second_batting = sum.second_batting;
+        this.second_bowling = sum.second_bowling;
+     });
+   }
 }
